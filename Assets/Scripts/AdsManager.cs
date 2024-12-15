10d187ed01f87bf434a1bfd8e7a51ea21edfeec7
Julián Lionel Etchevarne Quimbel <60628743+juetqui@users.noreply.github.com>
2024-12-15 16:12:22 -0300

    se agrega 2 ads nuevos, falta pulir

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -4,48 +4,92 @@ using UnityEngine.Advertisements;
 public class AdsManager : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsShowListener
 {
     [SerializeField] string gameID = "5743370";
-    [SerializeField] string adID = "Rewarded_Android";
+    [SerializeField] string adBannerID = "Banner_Android";
+    [SerializeField] string adInterstitialID = "Interstitial_Android";
+    [SerializeField] string adRewardedID = "Rewarded_Android";
     [SerializeField] private PlayerPrefsController _prefs;
 
-    private void Start()
+    void Awake()
     {
-        Advertisement.Initialize(gameID, true, this);
+        InitializeAds();
+    }
+
+    public void InitializeAds()
+    {
+        if (!Advertisement.isInitialized && Advertisement.isSupported)
+        {
+            Advertisement.Initialize(gameID, true, this);
+        }
     }
 
     public void OnInitializationComplete()
     {
-        Advertisement.Load(adID);
+        Advertisement.Load(adInterstitialID);
+        Advertisement.Load(adRewardedID);
+        LoadBanner();
     }
 
     public void OnInitializationFailed(UnityAdsInitializationError error, string message)
     {
-
+        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
     }
 
-    public void ShowAD()
+    public void LoadBanner()
+    {
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Banner.Load(adBannerID, new BannerLoadOptions
+        {
+            loadCallback = OnBannerLoaded,
+            errorCallback = OnBannerFailedToLoad
+        });
+    }
+
+    void OnBannerLoaded()
+    {
+        Advertisement.Banner.Show(adBannerID);
+        Debug.Log("Banner cargado y mostrado.");
+    }
+
+    void OnBannerFailedToLoad(string message)
+    {
+        Debug.LogError("Error al cargar el banner: " + message);
+    }
+
+    public void ShowInterstitialAD()
     {
         if (!Advertisement.isInitialized) return;
 
-        Advertisement.Show(adID, this);
+        Advertisement.Show(adInterstitialID, this);
     }
 
-    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    public void ShowRewardedAD()
     {
+        if (!Advertisement.isInitialized) return;
 
-    }
-
-    public void OnUnityAdsShowStart(string placementId)
-    {
-
-    }
-
-    public void OnUnityAdsShowClick(string placementId)
-    {
+        Advertisement.Show(adRewardedID, this);
     }
 
     public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
     {
-        if(showCompletionState == UnityAdsShowCompletionState.COMPLETED)
-            _prefs.SaveForAdd(30);
+        if (showCompletionState == UnityAdsShowCompletionState.COMPLETED)
+        {
+            if (placementId == "Rewarded_Android")
+                _prefs.SaveForAdd(30);
+            else if (placementId == "Interstitial_Android")
+                _prefs.SaveForAdd(30);
+        }
+        else return;
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
     }
 }
